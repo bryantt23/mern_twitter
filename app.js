015768d454b3d90b5d8500713996c174983e3ee4
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-15 01:00:01 -0700

    Connect to mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoUri;
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('Connected to mongoDB'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('hi');
