c9666efeabd7e0afd93f67087add1d2220607fb2
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-15 21:56:03 -0700

    User error messages

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -10,23 +10,19 @@ router.get('/', (req, res) => {
   Tweet.find()
     .sort({ date: -1 })
     .then(tweets => res.json(tweets))
-    .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
+    .catch(err => res.status(400).json(err));
 });
 
 router.get('/user/:user_id', (req, res) => {
   Tweet.find({ user: req.params.user_id })
     .then(tweets => res.json(tweets))
-    .catch(err =>
-      res.status(404).json({ notweetsfound: 'No tweets found from that user' })
-    );
+    .catch(err => res.status(400).json(err));
 });
 
 router.get('/:id', (req, res) => {
   Tweet.findById(req.params.id)
     .then(tweet => res.json(tweet))
-    .catch(err =>
-      res.status(404).json({ notweetfound: 'No tweet found with that ID' })
-    );
+    .catch(err => res.status(400).json(err));
 });
 
 router.post(
