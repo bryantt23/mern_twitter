1e7d9923fa64ee4b64f00a4b31623c26324e8df6
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-03-15 19:57:27 -0700

    Add validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,19 +4,26 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get('/test', (req, res) => {
   res.json({ msg: 'This is the user route' });
 });
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      // Throw a 400 error if the email address already exists
-      return res
-        .status(400)
-        .json({ email: 'A user has already registered with this address' });
+      // Use the validations to send the error
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
     } else {
       // Otherwise create a new user
       const newUser = new User({
@@ -40,12 +47,20 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({ email }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: 'This user does not exist' });
+      // Use the validations to send the error
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     bcrypt.compare(password, user.password).then(isMatch => {
